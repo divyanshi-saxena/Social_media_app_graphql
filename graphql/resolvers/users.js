ebb4a92d0f139194262a712123986fbd2af7dcc8
divyanshi-saxena <divyanshi.saxena0204@gmail.com>
2022-05-01 23:49:50 +0530

    Users and Posts resolvers added

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,13 +4,31 @@ const { UserInputError } = require("apollo-server");
 
 const User = require("../../models/User");
 const { SECRET_KEY } = require("../../config");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    {
+      expiresIn: "1h",
+    }
+  );
+}
 
 module.exports = {
   /**
    * register(parent, args, context, info)
    * parent - gives what was the input from last step - it is used when we have multiple resolvers
    * args - given as input, can be destructured
-   * context -
+   * context - we get req, res from express and this req contains body, authorization headers for authentication
    * info - metadata info which we almost never need
    */
   Mutation: {
@@ -18,8 +36,17 @@ module.exports = {
       _,
       { registerInput: { username, email, password, confirmPassword } }
     ) => {
-      // TODO: Validate user data
-      // TODO: Make sure user doesn't already exist
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      // Make sure user doesn't already exist
       const user = await User.findOne({ username });
 
       if (user) {
@@ -46,21 +73,39 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        {
-          expiresIn: "1h",
-        }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
         id: res._id,
+        token,
+      };
+    },
+
+    login: async (_, { username, password }) => {
+      const { errors, valid } = validateLoginInput(username, password);
+      const user = await User.findOne({ username });
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
         token,
       };
     },
